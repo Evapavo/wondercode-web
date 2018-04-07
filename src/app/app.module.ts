fd59df540a9a39c94eba23e87facf1a1c3f770c1
Evapavo <32174314+Evapavo@users.noreply.github.com>
2018-04-07 15:45:42 +0200

    feat: basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { routes } from './app.routes';
+import { NavbarComponent } from './components/misc/navbar/navbar.component';
+import { HomeComponent } from './components/misc/home/home.component';
+import { FooterComponent } from './components/misc/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,40 @@
+// import { CanLeavePhoneCreateGuard } from './shared/guards/can-leave-phone-create.guard';
+// import { PhoneBaseComponent } from './components/phone/phone-base/phone-base.component';
+// import { PhoneCreateComponent } from './components/phone/phone-create/phone-create.component';
+// import { PhoneDetailsResolverGuard } from './shared/resolvers/phone-details-resolver.guard';
+// import { IsAuthenticatedGuard } from './shared/guards/is-authenticated.guard';
+// import { SignupComponent } from './components/misc/signup/signup.component';
+// import { LoginComponent } from './components/misc/login/login.component';
+// import { PhoneItemComponent } from './components/phone/phone-item/phone-item.component';
+// import { PhoneListComponent } from './components/phone/phone-list/phone-list.component';
+import { HomeComponent } from './components/misc/home/home.component';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }
+    // { path: 'phones', canActivate: [IsAuthenticatedGuard], component: PhoneListComponent},
+    // {
+    //     path: 'phones',
+    //     canActivate: [IsAuthenticatedGuard],
+    //     component: PhoneBaseComponent,
+    //     children: [
+    //         {
+    //             path: 'new',
+    //             canActivate: [IsAuthenticatedGuard],
+    //             canDeactivate: [CanLeavePhoneCreateGuard],
+    //             component: PhoneCreateComponent
+    //         },
+    //         {
+    //             path: ':id',
+    //             canActivate: [IsAuthenticatedGuard],
+    //             resolve: {
+    //                 phone: PhoneDetailsResolverGuard
+    //             },
+    //             component: PhoneItemComponent
+    //         }
+    //     ]
+    // },
+    // { path: 'login', component: LoginComponent },
+    // { path: 'signup', component: SignupComponent },
+];
