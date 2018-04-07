1e579528c4308c1f2024a29fe927d16c2a23bd9d
Evapavo <32174314+Evapavo@users.noreply.github.com>
2018-04-07 17:28:54 +0200

    feat: login/signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import './rxjs.operators';
+
+import { IsAuthenticatedGuard } from './shared/guards/is-authenticated.guard';
+
+import { SessionService } from './shared/services/session.service';
+import { UsersService } from './shared/services/users.service';
 
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
 import { NavbarComponent } from './components/misc/navbar/navbar.component';
 import { HomeComponent } from './components/misc/home/home.component';
 import { FooterComponent } from './components/misc/footer/footer.component';
+import { LoginComponent } from './components/misc/login/login.component';
+import { SignupComponent } from './components/misc/signup/signup.component';
+import { SourcesComponent } from './components/sources/sources.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    SourcesComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    SessionService,
+    IsAuthenticatedGuard,
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,19 @@
 // import { PhoneBaseComponent } from './components/phone/phone-base/phone-base.component';
 // import { PhoneCreateComponent } from './components/phone/phone-create/phone-create.component';
 // import { PhoneDetailsResolverGuard } from './shared/resolvers/phone-details-resolver.guard';
-// import { IsAuthenticatedGuard } from './shared/guards/is-authenticated.guard';
-// import { SignupComponent } from './components/misc/signup/signup.component';
-// import { LoginComponent } from './components/misc/login/login.component';
+import { IsAuthenticatedGuard } from './shared/guards/is-authenticated.guard';
+import { SignupComponent } from './components/misc/signup/signup.component';
+import { LoginComponent } from './components/misc/login/login.component';
 // import { PhoneItemComponent } from './components/phone/phone-item/phone-item.component';
 // import { PhoneListComponent } from './components/phone/phone-list/phone-list.component';
+import { SourcesComponent } from './components/sources/sources.component';
 import { HomeComponent } from './components/misc/home/home.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'sources', canActivate: [IsAuthenticatedGuard], component: SourcesComponent },
     // { path: 'phones', canActivate: [IsAuthenticatedGuard], component: PhoneListComponent},
     // {
     //     path: 'phones',
@@ -35,6 +37,6 @@ export const routes: Routes = [
     //         }
     //     ]
     // },
-    // { path: 'login', component: LoginComponent },
-    // { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
 ];
