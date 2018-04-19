40494f439ae8c8096e2500f67681b98d9efc855a
Evapavo <32174314+Evapavo@users.noreply.github.com>
2018-04-19 21:39:46 +0200

    Auto-generated commit

--- src/app/components/source-base/source-base.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-source-base',
-  templateUrl: './source-base.component.html',
-
-})
-export class SourceBaseComponent {}
