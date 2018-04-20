45a8837fde3abcb4ddfb9cb25af495ac662ce5a8
Evapavo <32174314+Evapavo@users.noreply.github.com>
2018-04-20 15:20:11 +0200

    pre-heroku

--- src/app/shared/model/source.model.ts
+++ src/app/shared/model/source.model.ts
@@ -5,6 +5,7 @@ export class Source {
     sourceType: string;
     image?: string;
     text: string;
+    description: string;
 
 
     public asFormData(): FormData {
@@ -13,7 +14,8 @@ export class Source {
         data.append('name', this.name);
         data.append('sourceType', this.sourceType);
         data.append('image', this.image);
-        data.append('text', this.text)
+        data.append('text', this.text);
+        data.append('description', this.description);
 
         return data;
     }
